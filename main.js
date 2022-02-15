ea0620215d8b45c7a7fcca81bd1afe004d3496c3
Dana <danamudrova@gmail.com>
2022-02-15 18:52:42 +0100

    classlist add,remove

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 // tady je místo pro náš program
+
+//checklist jde do celé třídy
+//add - přidává třídu
+//remove - odebírá třídu
+let nadpis = document.querySelector('h1');
+nadpis.classList.add('zluty'); 
+nadpis.classList.remove('zluty');
+
+let ctverecek = document.querySelector('div.zluty');
+ctverecek.classList('ctverecek');
+
+//toggle - přidá se nebo odebere třída podle toho,co tam je
