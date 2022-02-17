cae037a074ebfbff6d1e664923298bacdb347466
Dana <danamudrova@gmail.com>
2022-02-17 21:52:33 +0100

    added ukol 1 and ukol3

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ ctverecek.classList.add('ctverecek');
 
 //toggle - přidá se nebo odebere třída podle toho,co tam je
 function zmenStyl() {
-nadpis.classList.toggle('zeleny')
+nadpis.classList.toggle('zeleny');
 
 }
 
@@ -29,15 +29,12 @@ function stiskKlavesy(udalost){
     console.log(udalost.key);
 }
 
-function startAudio() {
-    let audioFile = document.getElementById('zvukovaStopa');
-    audioFile.play();
-}
+
 
 //ukol breakoutroom
 
 function ztloustni() {
-    let odstavec = document.querySelector('p')
+    let odstavec = document.querySelector('p');
     odstavec.style.fontWeight= 'bold';
 }
 
@@ -48,10 +45,57 @@ function zhubni() {
 }
 //tip: funkce by měly mít let definovanou uvnitř a nikoli globálně,aby to bylo použitelné i pro jindy a jinde
 
-//úkol 1
+//úkol 1 
 
-function cervena() {
-
+function red() {
+    let pismo = document.querySelector('p');
+    pismo.classList.toggle('cerveny');
 }
 
 //úkol 2
+
+function big() {
+    
+    
+
+}
+
+//úkol 3
+
+//start
+
+function startAudio() {
+    let audioFile = document.getElementById('zvukovaStopa');
+    audioFile.play();
+}
+
+//pauza
+
+function pause(){
+    let audioFile = document.getElementById('zvukovaStopa');
+    audioFile.pause();
+}
+
+//hlasitosti
+function mute(){
+    let audioFile = document.getElementById('zvukovaStopa');
+    audioFile.volume = 0;
+}
+
+function medium(){
+    let audioFile = document.getElementById('zvukovaStopa');
+    audioFile.volume = 0.5;
+}
+
+function loud(){
+    let audioFile = document.getElementById('zvukovaStopa');
+    audioFile.volume = 1;
+}
+
+//opakování
+function repeat(){
+    let audioFile = document.getElementById('zvukovaStopa');
+    audioFile.pause();
+    audioFile.currentTime =0;
+    audioFile.play();
+}
