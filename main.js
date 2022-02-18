c86ec7382b88c1e2ed7a5286d271a8db4db930e4
Dana <danamudrova@gmail.com>
2022-02-18 21:13:55 +0100

    ukol 3 a procisteni

--- main.js
+++ main.js
@@ -1,35 +1,3 @@
-// tady je místo pro náš program
-
-//checklist jde do celé třídy
-//add - přidává třídu
-//remove - odebírá třídu
-let nadpis = document.querySelector('h1');
-nadpis.classList.add('zluty'); 
-nadpis.classList.remove('zluty');
-
-let ctverecek = document.querySelector('div.zluty');
-ctverecek.classList.add('ctverecek');
-
-//toggle - přidá se nebo odebere třída podle toho,co tam je
-function zmenStyl() {
-nadpis.classList.toggle('zeleny');
-
-}
-
-//udalosti
-let pocitadlo = 0;
-function priNajetiNaCtverec() {
-
-console.log('test'+pocitadlo);
-pocitadlo++;
-
-}
-// vlastnosti událostí
-function stiskKlavesy(udalost){
-    console.log(udalost.key);
-}
-
-
 
 //ukol breakoutroom
 
@@ -38,12 +6,10 @@ function ztloustni() {
     odstavec.style.fontWeight= 'bold';
 }
 
-
 function zhubni() {
     let odstavec = document.querySelector('p')
     odstavec.style.fontWeight= 'initial';
 }
-//tip: funkce by měly mít let definovanou uvnitř a nikoli globálně,aby to bylo použitelné i pro jindy a jinde
 
 //úkol 1 
 
@@ -55,9 +21,10 @@ function red() {
 //úkol 2
 
 function big() {
-    
-    
-
+    let pismo = document.getElementById('text');
+    let velikost = window.getComputedStyle(pismo, null).getPropertyValue('font-size');
+    let currentSize = parseFloat(velikost)
+    pismo.style.fontSize = (currentSize + 1) + 'px';
 }
 
 //úkol 3
